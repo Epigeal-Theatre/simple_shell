6af1076d6fa2b4658f30f8d25d8dd0dc241ec769
ziad <ziadabdelmabood@gmail.com>
2023-12-07 22:42:08 +0200

    fixed a lot of issues, mainly the newline character at the end of getline output, now working with PATH and local executables, no arguments

--- ziad/wip_ziad.c
+++ ziad/wip_ziad.c
@@ -8,13 +8,10 @@
  */
 int main(void)
 {
-	int /*i = 0, */exe_return, wstatus;
-	int /*shell_pid = getpid() ,*/child_pid;
+	int /*i = 0,*/ exe_return, wstatus, child_pid;
 	size_t read_bytes = 1024;
 	char delim[] = " ";
-	char *str_buf = NULL; 
-	char *token = NULL;
-	char *arg_buf[100] = {"-l", NULL};
+	char *str_buf = NULL, *token = NULL, *arg_buf, *exe_path = NULL;
 	
 	while (1)
 	{
@@ -23,43 +20,56 @@ int main(void)
 
 		/* accepting input into a string buffer */
 		getline(&str_buf, &read_bytes, stdin);
+		str_buf[strlen(str_buf) - 1] = '\0';
 
 		printf("getline worked and read: %s\n", str_buf);
 
-		token = malloc(strlen(str_buf) * sizeof(char));
-/* tokenizing the string so it turns into an array of arguments */
+		/* tokenizing the string so it turns into an array of arguments */
 		token = strtok(str_buf, delim);
 		printf("the first strtok worked and accepted: %s\n\n", token);
-		
-/*		while (token)
-		{
-			strcpy(arg_buf[i], token);
-			token = strtok(NULL, delim);
-			i++;
-		}
+		if (strcmp(token, "exit") == 0)
+			return (0);
 
-		arg_buf[i] = NULL;
+		exe_path = malloc(strlen(token));
+		strcpy(exe_path, token);
+		printf("exe_path = %s\n\n", exe_path);
+
+		arg_buf = exe_path;
+/*
+		arg_buf = malloc(strlen(token));
+		while (token != NULL)
+		{
+			printf("token = %s\narg_buf[i] = %s\ni = %i\n",
+					token, arg_buf[i], i);
+			strcpy(arg_buf[i], token);
+			printf("token = %s\narg_buf[i] = %s\ni = %i\n",
+					token, arg_buf[i], i);
+			token = strtok(NULL, delim);
+			printf("token = %s\narg_buf[i] = %s\ni = %i\n",
+					token, arg_buf[i], i);
+			i++;
+			arg_buf = realloc(arg_buf, sizeof(*arg_buf) + strlen(token));
+		}
+		arg_buf[i + 1] = NULL;
 
 		for (i = 0; arg_buf[i]; i++)
 			printf("strtok() worked and read: %s\n", arg_buf[i]);
-
-	*/
-/* freeing str_buf since we copied its tokens into arg_buf */
-	/*	free(str_buf);*/
+*/
 
 
-
+		/* freeing str_buf since we copied its tokens into arg_buf */
+		free(str_buf);
 
 		/* forking to execute the program */
 		child_pid = fork();
 		if (child_pid == 0)
 		{
 			printf("child process start\n");
-			exe_return = execve(arg_buf[0], arg_buf, NULL); 
+			printf("arg_buf = %s\n\n", arg_buf);
+			exe_return = execlp(exe_path, arg_buf, NULL); 
 			if (exe_return == -1)
 			{
 				perror("Error: ");
-				exit(-1);
 			}
 
 		}
@@ -67,8 +77,9 @@ int main(void)
 		{
 			wait(&wstatus);
 			printf("child process end and now in shell\n"); 
-		}
 
-	/*	free(str_buf);*/
+			/*freeing exe_path since execution is over*/
+			free(exe_path);
+		}
 	}
 }
