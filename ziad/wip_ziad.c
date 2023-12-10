5f79235738d1193dfe17d47cabf03ea5f0c0a98f
ziad <ziadabdelmabood@gmail.com>
2023-12-10 20:32:51 +0200

    improve on the shell program, saving arguments into an array works but child process reads bad data

--- ziad/wip_ziad.c
+++ ziad/wip_ziad.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-	int /*i = 0,*/ exe_return, wstatus, child_pid;
+	int i = 0, exe_return, wstatus, child_pid;
 	size_t read_bytes = 1024;
 	char delim[] = " ";
-	char *str_buf = NULL, *token = NULL, *arg_buf, *exe_path = NULL;
-	
+	char *str_buf = NULL, *token = NULL, *arg_buf[5], *exe_path = NULL;
+
 	while (1)
 	{
 		/* printing the prompt */
@@ -22,40 +22,35 @@ int main(void)
 		getline(&str_buf, &read_bytes, stdin);
 		str_buf[strlen(str_buf) - 1] = '\0';
 
-		printf("getline worked and read: %s\n", str_buf);
-
+		printf("\ngetline worked and read: %s\n\n", str_buf);
 		/* tokenizing the string so it turns into an array of arguments */
 		token = strtok(str_buf, delim);
 		printf("the first strtok worked and accepted: %s\n\n", token);
 		if (strcmp(token, "exit") == 0)
 			return (0);
 
+		/* saving path and name of program into exe_path */
 		exe_path = malloc(strlen(token));
 		strcpy(exe_path, token);
 		printf("exe_path = %s\n\n", exe_path);
 
-		arg_buf = exe_path;
-/*
-		arg_buf = malloc(strlen(token));
+		/* creating an argument array by tokenizing the input string */
 		while (token != NULL)
 		{
-			printf("token = %s\narg_buf[i] = %s\ni = %i\n",
+			printf("[1] token = %s\narg_buf[i] = %s\ni = %i\n",
 					token, arg_buf[i], i);
-			strcpy(arg_buf[i], token);
-			printf("token = %s\narg_buf[i] = %s\ni = %i\n",
+			arg_buf[i] = token;
+			printf("[2] token = %s\narg_buf[i] = %s\ni = %i\n",
 					token, arg_buf[i], i);
 			token = strtok(NULL, delim);
-			printf("token = %s\narg_buf[i] = %s\ni = %i\n",
+			printf("[3] token = %s\narg_buf[i] = %s\ni = %i\n",
 					token, arg_buf[i], i);
 			i++;
-			arg_buf = realloc(arg_buf, sizeof(*arg_buf) + strlen(token));
 		}
 		arg_buf[i + 1] = NULL;
 
 		for (i = 0; arg_buf[i]; i++)
 			printf("strtok() worked and read: %s\n", arg_buf[i]);
-*/
-
 
 		/* freeing str_buf since we copied its tokens into arg_buf */
 		free(str_buf);
@@ -64,19 +59,16 @@ int main(void)
 		child_pid = fork();
 		if (child_pid == 0)
 		{
-			printf("child process start\n");
+			printf("child process start\n\n");
 			printf("arg_buf = %s\n\n", arg_buf);
-			exe_return = execlp(exe_path, arg_buf, NULL); 
+			exe_return = execvp(exe_path, arg_buf);
 			if (exe_return == -1)
-			{
 				perror("Error: ");
-			}
-
 		}
 		else
 		{
 			wait(&wstatus);
-			printf("child process end and now in shell\n"); 
+			printf("child process end and now in shell\n");
 
 			/*freeing exe_path since execution is over*/
 			free(exe_path);
