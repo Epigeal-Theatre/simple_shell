488a71d66800015131af46b71e6dc380340e729d
ziad <ziadabdelmabood@gmail.com>
2023-12-15 12:13:10 +0200

    add modular tokenization funciton with dynamic memory allocation

---
+++ functions.c
@@ -0,0 +1,102 @@
+#include "shell.h"
+
+/**
+ * tokenarray -	tokenizes a string into an array of arguments.
+ * @str:	string to tokenize.
+ * @arg:	argument array.
+ * return:	pointer to argument array.
+ */
+char **tokenarray(char *str)
+{
+	int i = 0;
+	char **arg = NULL;
+	char *token = NULL;
+	char delim[] = " ";
+
+	token = strtok(str, delim);
+	if (_strcmp(token, "exit") == 0)
+		successandfree(str, NULL);
+
+	arg = malloc(sizeof(char *) * 2);
+	if (arg == NULL)
+		errorandfree(str, NULL);
+
+	arg[0] = malloc(strlen(token) + 1);
+	if (arg[0] == NULL)
+		errorandfree(str, NULL);
+	_strncpy(arg[0], token, (strlen(token) + 1));
+	
+	for (i = 1; token != NULL && i < ARG_LIMIT; i++)
+	{
+		token = strtok (NULL, delim);
+		
+		printf("strtok success, token = %s\n", token);
+
+		if (token)
+		{
+			arg = realloc(arg, sizeof(char*) * (i + 2));
+			if (arg == NULL)
+				errorandfree(str, arg);
+			arg[i] = malloc(strlen(token) + 1);
+			if (arg[i] == NULL)
+				errorandfree(str,arg);
+		
+			printf("arg[i] malloc success, i = %i\naddress of arg[i] = %p\n", i, arg[i]);
+
+			_strncpy(arg[i], token, (strlen(token) + 1));
+
+			printf("strncpy success, i = %i\n", i);
+		}
+	}
+
+	printf("loop exits successfuly\n");
+
+	arg[i] = NULL;
+
+	printf("arg[i] = NULL success\narg address = %p\n", *arg);
+	return (arg);
+}
+
+/**
+ * errorandfree -	prints error message and frees 2 buffers.
+ * @buf1:	first buffer.
+ * @buf2:	second buffer(can be NULL).
+ * return:	nothing.
+ */
+void errorandfree(char *buf1, char **buf2)
+{
+	int i;
+
+	perror("Error: ");
+	fflush(stdin);
+	free(buf1);
+	if (buf2)
+	{
+		for (i = 0; buf2[i]; i++)
+			free(buf2[i]);
+		free(buf2);
+	}
+
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * successandfree -	frees 2 buffers and exits.
+ * @buf1:	first buffer.
+ * @buf2:	second buffer(can be NULL).
+ * return:	nothing.
+ */
+void successandfree(char *buf1, char **buf2)
+{
+	int i;
+
+	fflush(stdin);
+	free(buf1);
+	if (buf2)
+	{
+		for (i = 0; buf2[i]; i++)
+			free(buf2[i]);
+		free(buf2);
+	}
+	exit(EXIT_SUCCESS);
+}
--- main.c
+++ main.c
@@ -1,20 +1,15 @@
 #include "shell.h"
 
-#define PROMPT "$ "
-#define ARG_LIMIT 10
-#define ERR_NOTFOUND "Error: file or command not found\n"
-
 /**
  * main -	simple shell program
  * Return:	0 at success.
  */
 int main(int ac, char **av, char **env)
 {
-	int i = 0, exe_return, wstatus, child_pid;
+	int exe_return, wstatus, child_pid;
 	size_t read_bytes = 1024;
 	ssize_t read = 0;
-	char delim[] = " ";
-	char *str_buf = NULL, *token = NULL, *arg_buf[ARG_LIMIT], *exe_path = NULL;
+	char *str_buf = NULL, **arg_buf; 
 	(void) ac;
 	(void) av;
 
@@ -22,86 +17,37 @@ int main(int ac, char **av, char **env)
 	{
 		if (isatty(0) == 1)
 			write(1, PROMPT, _strlen(PROMPT));
+
 		read = getline(&str_buf, &read_bytes, stdin);
-
 		if (read == -1)
-			successandfree(str_buf, exe_path);
-
+			successandfree(str_buf, arg_buf);
 		if (str_buf[_strlen(str_buf) - 1] == '\n')
 			str_buf[_strlen(str_buf) - 1] = '\0';
-
-		token = strtok(str_buf, delim);
-		if (!token)
+		if (!str_buf || read == 1)
 		{
 			fflush(stdin);
 			str_buf = NULL;
 			continue;
 		}
 
-		if (_strcmp(token, "exit") == 0)
-			successandfree(str_buf, NULL);
-
-		exe_path = malloc(strlen(token) + 1);
-		if (exe_path == NULL)
-			errorandfree(str_buf, NULL);
-
-		_strncpy(exe_path, token, (strlen(token) + 1));
-		i = 0;
-		while (token != NULL && i < ARG_LIMIT)
-		{
-			arg_buf[i] = token;
-			token = strtok(NULL, delim);
-			i++;
-		}
-		arg_buf[i] = NULL;
+		arg_buf = tokenarray(str_buf);
 
 		child_pid = fork();
 		if (child_pid == -1)
-			errorandfree(str_buf, exe_path);
+			errorandfree(str_buf, arg_buf);
 
 		if (child_pid == 0)
 		{
-			exe_return = execvpe(exe_path, arg_buf, env);
+			exe_return = execvpe(arg_buf[0], arg_buf, env);
 			if (exe_return == -1)
-				errorandfree(exe_path, str_buf);
+				errorandfree(str_buf, arg_buf);
 		}
 		else
 		{
 			wait(&wstatus);
-			exe_path = NULL;
+			fflush(stdin);
 			str_buf = NULL;
 		}
 	}
 	return (0);
-}
-
-/**
- * errorandfree -	prints error message and frees 2 buffers.
- * @buf1:		first buffer.
- * @buf2:		second buffer(can be NULL).
- * return:		nothing.
- */
-void errorandfree(char *buf1, char *buf2)
-{
-	perror("Error: ");
-	fflush(stdin);
-	free(buf1);
-	if (buf2)
-		free(buf2);
-	exit(EXIT_FAILURE);
-}
-
-/**
- * successandfree -	frees 2 buffers and exits.
- * @buf1:		first buffer.
- * @buf2:		second buffer(can be NULL).
- * return:		nothing.
- */
-void successandfree(char *buf1, char *buf2)
-{
-	fflush(stdin);
-	free(buf1);
-	if (buf2)
-		free(buf2);
-	exit(EXIT_SUCCESS);
 }
